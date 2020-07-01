10dad342159f77e78b7f67481821182d2736977f
KwongTN <tungnan5636@gmail.com>
2020-07-01 17:23:21 +0800

    Settling code rebase mess

--- src/params.h
+++ src/params.h
@@ -19,15 +19,6 @@ const int SEL_SIZE = 8;
 // Population size
 const int POP_SIZE = 10;
 
-// Gene size
-const int GENE_SIZE = 4;
-
-// Selection size
-const int SEL_SIZE = 8;
-
-// Population size
-const int POP_SIZE = 10;
-
 /*
  Define available characters, with the following conversion convention:
  - 1	: Red
